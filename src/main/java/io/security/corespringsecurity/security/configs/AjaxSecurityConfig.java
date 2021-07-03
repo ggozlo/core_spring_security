f64c2adfc2c2200bdcedc4b589cbfe7fa3fa14ab
ggozlo <ggozlo@naver.com>
2021-07-04 03:58:38 +0900

    CustomDsl( not essential)

--- src/main/java/io/security/corespringsecurity/security/configs/AjaxSecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/AjaxSecurityConfig.java
@@ -47,10 +47,10 @@ public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .antMatcher("/api/**")
                 .authorizeRequests()
+                .antMatchers("/api/login").permitAll()
                 .antMatchers("/api/messages").hasRole("MANAGER")
                 .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
+
         // 필터를 특정 위치에 추가, formLogin 은 Api 에서 기본 제공하는 필터로 여기서 새부지정을 했지만
         // ajax 로그인은 직접 구현한 필터임으로 해당 필터에 넣어야함
         ;
@@ -58,20 +58,36 @@ public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling()
                 .authenticationEntryPoint(new AjaxLoginAuthenticationEntryPoint())
                 .accessDeniedHandler(ajaxAccessDeniedHandler());
+//                .and()
+//                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
         http.csrf().disable();
+
+        customConfigurerAjax(http);
+        // 실제 적용
     }
+
+    private void customConfigurerAjax(HttpSecurity http) throws Exception {
+        http
+                .apply(new AjaxLoginConfigurer<>())
+                // 이부분에서 생성자와 init 메서드가 호출됨
+                .successHandlerAjax(ajaxAuthenticationSuccessHandler())
+                .failureHandlerAjax(ajaxAuthenticationFailureHandler())
+                .setAuthenticationManager(authenticationManagerBean())
+                .loginProcessingUrl("/api/login");
+                 //  createLoginProcessingUrlMatcher 메서드를 호출하여 antMatcher 에 적용될 주소를 넣어준디
+    } // 필터를 설정하는 API 를 직접 구현하 기존과 같은 기능을 하는 필터를 별도로 구성하게 되었다
 
     private AccessDeniedHandler ajaxAccessDeniedHandler() {
         return new AjaxAccessDeniedHandler();
     }
-
-    @Bean
-    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
-        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
-        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean()); // 필터에 인증 매니저 매칭칭        ajaxLoginProcessingFilter.setAuthenticationSuccessHandler(ajaxAuthenticationSuccessHandler());
-        ajaxLoginProcessingFilter.setAuthenticationFailureHandler(ajaxAuthenticationFailureHandler());
-        return ajaxLoginProcessingFilter;
-        // 필터에 핸들러 추가 등 세부 설정
-    }
+//
+//    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
+//        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
+//        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean()); // 필터에 인증 매니저 매칭칭
+//         ajaxLoginProcessingFilter.setAuthenticationSuccessHandler(ajaxAuthenticationSuccessHandler());
+//        ajaxLoginProcessingFilter.setAuthenticationFailureHandler(ajaxAuthenticationFailureHandler());
+//        return ajaxLoginProcessingFilter;
+//        // 필터에 핸들러 추가 등 세부 설정
+//    }
 
 }
